b89be5e472b71dbd1e22334925742509efc7c20f
nadolubogdan <nadolubogdan13@gmail.com>
2017-03-24 15:39:51 +0200

    implemented tasks for laboratory 05

--- labtasks/BoundedService/BoundedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/boundedserviceactivity/view/BoundedServiceActivity.java
+++ labtasks/BoundedService/BoundedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/boundedserviceactivity/view/BoundedServiceActivity.java
@@ -1,9 +1,18 @@
 package ro.pub.cs.systems.eim.lab05.boundedserviceactivity.view;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.sql.Timestamp;
 
 import ro.pub.cs.systems.eim.lab05.boundedserviceactivity.R;
 import ro.pub.cs.systems.eim.lab05.boundedserviceactivity.general.Constants;
@@ -18,31 +27,67 @@ public class BoundedServiceActivity extends AppCompatActivity {
     private int boundedServiceStatus = Constants.SERVICE_STATUS_UNBOUND;
 
     // TODO: exercise 10e - implement a button click listener for getMessageFromServiceButton
+    private GetMessageFromServiceButtonListener getMessageFromServiceButtonListener = new GetMessageFromServiceButtonListener();
+    private class GetMessageFromServiceButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if (boundedService != null && boundedServiceStatus == Constants.SERVICE_STATUS_BOUND) {
+                messageFromServiceTextView.setText("[" + new Timestamp(System.currentTimeMillis())
+                        + "] " + boundedService.getMessage()
+                        + "\n" + messageFromServiceTextView.getText().toString());
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bounded_service);
 
-        messageFromServiceTextView = (TextView)findViewById(R.id.message_from_service_text_view);
-        getMessageFromServiceButton = (Button)findViewById(R.id.get_message_from_service_button);
+        messageFromServiceTextView = (TextView) findViewById(R.id.message_from_service_text_view);
+        getMessageFromServiceButton = (Button) findViewById(R.id.get_message_from_service_button);
 
         // TODO: exercise 10e - set an instance of the button click listener to handle click events
         // for getMessageFromServiceButton
+        getMessageFromServiceButton.setOnClickListener(getMessageFromServiceButtonListener);
+
     }
+
+    // TODO: exercise 10c - create a ServiceConnection object
+    // override methods onServiceConnected() and onServiceDisconnected()
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            Log.d(Constants.TAG, "onServiceConnected() method was invoked");
+            BoundedService.BoundedServiceBinder binder = (BoundedService.BoundedServiceBinder) service;
+            boundedService = binder.getService();
+            boundedServiceStatus = Constants.SERVICE_STATUS_BOUND;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName className) {
+            Log.d(Constants.TAG, "onServiceDisconnected() method was invoked");
+            boundedService = null;
+            boundedServiceStatus = Constants.SERVICE_STATUS_UNBOUND;
+        }
+    };
 
     @Override
     protected void onStart() {
         super.onStart();
         // TODO: exercise 10d - bind the service through an intent
+        Intent intent = new Intent(this, BoundedService.class);
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
     }
 
     @Override
-    protected  void onStop() {
+    protected void onStop() {
         // TODO: exercise 10d - unbind the service
         super.onStop();
+        if (boundedServiceStatus == Constants.SERVICE_STATUS_BOUND) {
+            unbindService(serviceConnection);
+            boundedServiceStatus = Constants.SERVICE_STATUS_UNBOUND;
+        }
     }
 
-    // TODO: exercise 10c - create a ServiceConnection object
-    // override methods onServiceConnected() and onServiceDisconnected()
 }
